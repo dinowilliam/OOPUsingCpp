49e5d5f16ce276478f84d18206afc17f9cabb280
dinowilliam <dinowilliam.dev@gmail.comm>
2020-07-18 00:45:42 -0300

    Add tstack.cpp

---
+++ tstack.cpp
@@ -0,0 +1,60 @@
+/************************************************************************************************************************************/
+/*  Object-Oriented Programming Using C++  by Ira Pohl - Second Edition                                                             */
+/*  Program: tstack.cpp                                                                                                            */
+/************************************************************************************************************************************/
+
+//string3 in C++
+#include <iostream>        //IO library
+#include <string>          //string stype
+#include <complex>         //string complex type
+#include <cmath>           //string maths types
+
+using namespace std;      //standart library's namespace
+
+//Overloading the operator+.
+template <class TYPE>
+class stack {
+
+    public:
+        explicit stack(int size = 1000) : max_len(size)
+            { s = new TYPE[size]; top = EMPTY; }
+        ~stack() { delete []s; }
+        void resetO { top = EMPTY; }
+        void push (TYPE c) { s[++top] = c; }
+        TYPE pop() { return s[top--]; }
+        TYPE top_of() { return s[top]; }
+        bool empty() { return (top == EMPTY); }
+        bool full () {return (top == max_len-l);}
+    
+    private:
+        enum {EMPTY = -1};
+        TYPE* s;
+        int max_len;
+        int top;
+};
+
+stack<char> stk_ch; // 1000 char stack
+stack<char*> stk_str(200); // 200 char- stack
+//stack<complex> stk_cmplx(100); // 100 complex stack
+
+//Reversing a series of char- represented strings
+void reverse(char* str[], int n){
+
+    stack<char*> stk(n); //this stack holds char*
+    for (int i = 0; i < n; ++i)
+        stk.push(str[i]);
+
+    for (int i = 0; i < n; ++i)
+        str[i] = stk.pop() ;
+} 
+
+//Initializing stack of complex numbers from an array
+void init(complex c[], stack<complex>& stk, n){
+
+    for (int i = 0; i < n; ++i)
+        stk.push(c[i]);
+}
+
+int main(){
+    
+}
